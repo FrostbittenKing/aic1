eb6d5eda2cd65f138a729d812a0c46afbebd13fa
Hubert Hirsch <vevusio@silver.(none)>
2011-11-08 17:47:46 +0100

    rating service rest annotations

--- aic1services/src/aic/rating/RatingService.java
+++ aic1services/src/aic/rating/RatingService.java
@@ -2,11 +2,20 @@ package aic.rating;
 
 import aic.domain.dto.RequestRatings;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Produces("application/json")
+@Path("rating")
 public interface RatingService {
 	/**
 	 * Get the ratings for a given request
 	 * @param requestId
 	 * @return The ratings of the requests customer and his warrantors
 	 */
-	public RequestRatings getRequestRatings(long requestId);
+    @GET
+    @Path("{id}")
+	public RequestRatings getRequestRatings(@PathParam("id") long requestId);
 }
