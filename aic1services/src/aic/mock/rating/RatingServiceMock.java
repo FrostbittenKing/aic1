da3f8cd9a72717c645b41914e8fab524de324c2f
Grillkohle <Grillkohle@.(none)>
2011-11-08 19:11:55 +0100

    EMPTY COMMIT MESSAGE

--- aic1services/src/aic/mock/rating/RatingServiceMock.java
+++ aic1services/src/aic/mock/rating/RatingServiceMock.java
@@ -5,9 +5,4 @@ import java.util.HashMap;
 import aic.domain.dto.RequestRatings;
 
 public class RatingServiceMock {
-	//private HashMap<long, Request>
-	
-	public RequestRatings getRating(){
-		
-	}
 }
